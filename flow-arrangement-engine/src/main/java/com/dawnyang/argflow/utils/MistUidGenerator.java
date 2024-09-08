5ab6a56dc056da96931e06d33a1fcba69b34279e
Dawn Yang <dawn112358@outlook.com>
2024-09-08 10:50:34 +0800

    默认迷雾UID算法改为懒加载

--- flow-arrangement-engine/src/main/java/com/dawnyang/argflow/utils/MistUidGenerator.java
+++ flow-arrangement-engine/src/main/java/com/dawnyang/argflow/utils/MistUidGenerator.java
@@ -2,6 +2,7 @@ package com.dawnyang.argflow.utils;
 
 import com.dawnyang.argflow.api.UidGenerator;
 
+import java.util.Objects;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -14,25 +15,34 @@ import java.util.concurrent.atomic.AtomicLong;
  */
 public class MistUidGenerator implements UidGenerator {
 
-    private static final long START = 1704038400000L; //2024-01-01 00:00:00
-    private static final AtomicLong inc = new AtomicLong(System.currentTimeMillis() - START);
-    private static final UidGenerator instance = new MistUidGenerator();
+    private final long START = 1704038400000L; //2024-01-01 00:00:00
+    private final AtomicLong inc = new AtomicLong(System.currentTimeMillis() - START);
+    private static UidGenerator instance;
 
-    private MistUidGenerator(){}
+    private MistUidGenerator() {}
 
-    public static UidGenerator getInstance(){
+    public static UidGenerator getInstance() {
+        // 懒加载单例
+        if (Objects.isNull(instance)) {
+            synchronized (MistUidGenerator.class) {
+                if (Objects.isNull(instance)){
+                    instance = new MistUidGenerator();
+                    return instance;
+                }
+            }
+        }
         return instance;
     }
 
     @Override
-    public long getUid(){
+    public long getUid() {
         int r1 = ThreadLocalRandom.current().nextInt(256);
         int r2 = ThreadLocalRandom.current().nextInt(256);
         long current = inc.incrementAndGet();
         long currentTime = System.currentTimeMillis() - START;
-        if (current < currentTime){
-            synchronized (MistUidGenerator.class){
-                if (inc.get() < currentTime){
+        if (current < currentTime) {
+            synchronized (MistUidGenerator.class) {
+                if (inc.get() < currentTime) {
                     inc.set(currentTime);
                 }
             }
